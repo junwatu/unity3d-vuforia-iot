cbbefa996dd1bc6d0687278e5231858d4cb0f75e
Equan Pr <equan.p@gmail.com>
2018-03-24 21:42:32 +0700

    Set lamp state on or off

--- Assets/Scripts/LampState.cs
+++ Assets/Scripts/LampState.cs
@@ -2,23 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LampState : MonoBehaviour {
+public class LampState : MonoBehaviour
+{
 
     private GameObject gameObjectLamp;
-	private Material m_Material;
-	public Material change_Material;
-	public Material default_Material;
+    private Material m_Material;
+    public Material change_Material;
+    public Material default_Material;
 
-	void Start() {
+    void Start()
+    {
         m_Material = GetComponent<Renderer>().material;
-	}
+    }
 
-	public void ChangeColorON() {
-		Debug.Log("Button is pressed!");
-		gameObject.GetComponent<Renderer>().material = change_Material;
-	}
+    public void ChangeColorON()
+    {
+        Debug.Log("Button is pressed!");
+        gameObject.GetComponent<Renderer>().material = change_Material;
 
-	public void ChangeColorOFF() {
-		gameObject.GetComponent<Renderer>().material = default_Material;
-	}
+        string json = "{\"status\":\"true\"}";
+		
+        StartCoroutine(RESTClient.LampState(json));
+    }
+
+    public void ChangeColorOFF()
+    {
+        gameObject.GetComponent<Renderer>().material = default_Material;
+    }
 }
