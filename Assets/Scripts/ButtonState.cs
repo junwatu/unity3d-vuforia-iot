60bcbcc7dcdb52b35f1872e9baac3e994bf82d05
Equan Pr <equan.p@gmail.com>
2018-03-24 14:31:13 +0700

    Update project settings

---
+++ Assets/Scripts/ButtonState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonState : MonoBehaviour {
+
+	public void ChangeColor() {
+		Debug.Log("Button ON OFF Change State");
+	}
+}
