e012c283ad9573fe1e8d2479747c0b060c9a2614
Equan Pr <equan.p@gmail.com>
2018-03-24 13:12:00 +0700

    Add assets for lamp mmodel

---
+++ Assets/Scripts/LampState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampState : MonoBehaviour {
+
+    private GameObject gameObjectRing;
+	private Material m_Material;
+
+	public float speed = 50.0f;
+	public Material change_Material;
+	public Material default_Material;
+
+	void Start() {
+        m_Material = GetComponent<Renderer>().material;
+	}
+	
+	void Update() {
+		transform.Rotate(Vector3.back * Time.deltaTime * speed);
+	}
+
+	public void ChangeColorGold() {
+		Debug.Log("Button is pressed!");
+		gameObject.GetComponent<Renderer>().material = change_Material;
+	}
+
+	public void ChangeColorDark() {
+		gameObject.GetComponent<Renderer>().material = default_Material;
+	}
+}
