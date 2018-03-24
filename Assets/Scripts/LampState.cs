607581c6f75c5fdac4210fb7ff3caee6ce63d93c
Equan Pr <equan.p@gmail.com>
2018-03-24 14:30:54 +0700

    Change lamp state based on button interaction

--- Assets/Scripts/LampState.cs
+++ Assets/Scripts/LampState.cs
@@ -4,27 +4,21 @@ using UnityEngine;
 
 public class LampState : MonoBehaviour {
 
-    private GameObject gameObjectRing;
+    private GameObject gameObjectLamp;
 	private Material m_Material;
-
-	public float speed = 50.0f;
 	public Material change_Material;
 	public Material default_Material;
 
 	void Start() {
         m_Material = GetComponent<Renderer>().material;
 	}
-	
-	void Update() {
-		transform.Rotate(Vector3.back * Time.deltaTime * speed);
-	}
 
-	public void ChangeColorGold() {
+	public void ChangeColorON() {
 		Debug.Log("Button is pressed!");
 		gameObject.GetComponent<Renderer>().material = change_Material;
 	}
 
-	public void ChangeColorDark() {
+	public void ChangeColorOFF() {
 		gameObject.GetComponent<Renderer>().material = default_Material;
 	}
 }
