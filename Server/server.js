acb3345a22ce16224826beb068f5812570ce48e6
Equan Pr <equan.p@gmail.com>
2018-03-25 06:17:19 +0700

    Add notifier

--- Server/server.js
+++ Server/server.js
@@ -1,26 +1,32 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const notifier = require('node-notifier')
 
 const jsonParser = bodyParser.json();
+
+const port = 3003;
 
 app.get('/', (req, res) => { res.send('REST Server') })
 
 app.post('/api/lamp', jsonParser, (req, res) => {
-    
-    console.log(req.body);
 
     if (!req.body) return res.sendStatus(400)
-    
+
     let status = req.body.status
 
     if (!req.body.status) {
-        console.log(status)
         res.send('status empty')
     } else {
-        console.log(status)
+        // send data to control device (Arduiono, etc) 
+        // Simple notification
+        notifier.notify({
+            title: 'Lamp State',
+            message: status
+        });
+        //
         res.send('OK')
     }
 })
 
-app.listen(3003, () => console.log('Started')) 
+app.listen(port, () => console.log(`REST server on ${port}`)) 
